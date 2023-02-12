38a63eec96d4fafab421b973487582bddaf2cb8d
Abner Tarso <abnertarsolima@gmail.com>
2023-02-12 17:21:52 -0300

    menu mobile refatorado

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,20 +1,35 @@
-import handleOutside from './outside.js'
-export default function initMenuMobile() {
-    const menu = document.querySelector('[data-menu]');
-    const menuList = document.querySelector('[data-menuList]');
-    const eventos = ['touchstart', 'click'];
-    if(menuList) {
-        function startMenu() {
-            menu.classList.add('active');
-            menuList.classList.add('active');
-            handleOutside(menuList, ['click', 'touchstart'] ,() => {
-                menu.classList.remove('active');
-                menuList.classList.remove('active');
-            })
-        }
+import handleOutside from './outside.js';
 
-        eventos.forEach((userEvent) => {
-            menu.addEventListener(userEvent, startMenu);
+export default class MenuMobile {
+    constructor(menu, menuList, event) {
+        this.menu = document.querySelector(menu);
+        this.menuList = document.querySelector(menuList);
+        this.activeClass = 'active';
+
+        if (this.events === undefined) this.eventos = ['touchstart', 'click'];
+        else this.events = event;
+
+        this.startMenu = this.startMenu.bind(this);
+    }
+    
+    startMenu() {
+        this.menu.classList.add(this.activeClass);
+        this.menuList.classList.add(this.activeClass);
+        handleOutside(this.menuList, this.events, () => {
+            this.menu.classList.remove(this.activeClass);
+            this.menuList.classList.remove(this.activeClass);
+        })
+    }
+
+    addEvent() {
+        this.events.forEach((userEvent) => {
+            this.menu.addEventListener(userEvent, this.startMenu);
         });
+    }
+
+    init() {
+        if(this.menu && this.menuList) {
+            this.addEvent();
+        }
     }
 }
