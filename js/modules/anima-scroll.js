09006bb15dd5e267aaef52fdc29045def276cae4
Abner Tarso dos Santos <abner@policiacivil.local>
2023-07-31 04:07:53 -0300

    refatorando o projeto e colocando o menu fixo

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -1,12 +1,20 @@
+import debounce from "./debounce";
+
 export default class AnimaScroll {
     constructor(scroll) {
         this.scrollSuave = document.querySelectorAll(scroll);
         this.windowMetade = window.innerHeight * .8;
         this.eventClass = 'ativo';
-        this.startScroll = this.startScroll.bind(this);
+        this.startScroll = debounce(this.startScroll.bind(this), 100);
     }
 
     startScroll() {
+        const cabecalho = document.querySelector('.nav-header');
+        const comunicate = document.querySelector('.box2');
+        const comunicateT = document.querySelector('.box3');
+        if(comunicate.classList.contains('ativo')) {
+            cabecalho.classList.add('ativo');
+        }
         this.scrollSuave.forEach((event) => {
             const scroller = event.getBoundingClientRect().top;
             const conta = (scroller - this.windowMetade) < 0;
